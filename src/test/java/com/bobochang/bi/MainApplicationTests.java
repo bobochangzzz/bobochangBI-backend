4c7cd4ab4b870a37010e0759a92c50887684d483
bobochang <m13726750698_2@163.com>
2023-05-21 23:52:44 +0800

    feat: 初始化项目

---
+++ src/test/java/com/bobochang/bi/MainApplicationTests.java
@@ -0,0 +1,26 @@
+package com.bobochang.bi;
+
+import com.bobochang.bi.config.WxOpenConfig;
+
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ *
+ * @author <a href="https://github.com/libobochang">程序员鱼皮</a>
+ * @from <a href="https://bobochang.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Resource
+    private WxOpenConfig wxOpenConfig;
+
+    @Test
+    void contextLoads() {
+        System.out.println(wxOpenConfig);
+    }
+
+}
---
+++ src/test/java/com/bobochang/bi/mapper/PostMapperTest.java
@@ -0,0 +1,29 @@
+package com.bobochang.bi.mapper;
+
+import com.bobochang.bi.model.entity.Post;
+
+import java.util.Date;
+import java.util.List;
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 帖子数据库操作测试
+ *
+ * @author <a href="https://github.com/libobochang">程序员鱼皮</a>
+ * @from <a href="https://bobochang.icu">编程导航知识星球</a>
+ */
+@SpringBootTest
+class PostMapperTest {
+
+    @Resource
+    private PostMapper postMapper;
+
+    @Test
+    void listPostWithDelete() {
+        List<Post> postList = postMapper.listPostWithDelete(new Date());
+        Assertions.assertNotNull(postList);
+    }
+}
